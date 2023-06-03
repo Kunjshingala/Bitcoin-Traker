e370cb0677bf9d76e9ae430d27fec90b0b9a88f6
Kunjshingala <kunjshingla.p@gmail.com>
2023-06-03 18:38:00 +0530

    show 3 crypto's value in more than 10 currency.

---
+++ lib/coin_data.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'INR',
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR',
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = "https://rest.coinapi.io/v1/exchangerate";
+const apikey = "B04D3611-B390-46A2-A72D-47983D432408";
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apikey';
+      Uri url = Uri.parse(requestURL);
+      http.Response response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        var decodeData = jsonDecode(response.body);
+        print("=================================" + response.body);
+        var lastPrice = decodeData['rate'];
+        return lastPrice;
+      } else {
+        print(response.statusCode);
+        throw 'Problem with get request';
+      }
+    }
+  }
+}
---
+++ lib/price_screen.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'dart:io' show Platform;
+
+class PriceScreen extends StatefulWidget {
+  const PriceScreen({super.key});
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'INR';
+
+  // selector for Android
+  DropdownButton<dynamic> androidDropdown() {
+    List<DropdownMenuItem> dropdownItems = [];
+    for (String currency in currenciesList) {
+      var newItem = DropdownMenuItem(
+        value: currency,
+        child: Text(currency),
+      );
+      dropdownItems.add(newItem);
+    }
+    return DropdownButton<dynamic>(
+      value: selectedCurrency,
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(
+          () {
+            selectedCurrency = value;
+            getData();
+            print(selectedCurrency);
+          },
+        );
+      },
+    );
+  }
+
+  // selector for Ios
+  CupertinoPicker iosPicker() {
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+    return CupertinoPicker(
+        itemExtent: 32,
+        onSelectedItemChanged: (selectedIndex) {
+          print(selectedIndex);
+          setState(
+            () {
+              selectedCurrency = currenciesList[selectedIndex];
+              getData();
+            },
+          );
+        },
+        children: pickerItems);
+  }
+
+  // check IOS OR Android
+  Widget getPicker() {
+    if (Platform.isIOS) {
+      return iosPicker();
+    } else if (Platform.isAndroid) {
+      return androidDropdown();
+    } else {
+      return iosPicker();
+    }
+  }
+
+  Map<String, String> coinValues = {};
+  bool isWaiting = false;
+
+  void getData() async {
+    isWaiting = true;
+    try {
+      double data = await CoinData().getCoinData(selectedCurrency);
+      isWaiting = false;
+      setState(() {
+        coinValues = data as Map<String, String>;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // getDropdownItems();
+    return Scaffold(
+      appBar: AppBar(
+        // title: Text('🤑 Coin Ticker'),
+        title: const Center(
+          child: Text('🤑 Coin Ticker'),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CryptoCard(
+                cryptoCurrency: 'BTC',
+                value: isWaiting ? '?' : coinValues['BTC'],
+                selectedCurrency: selectedCurrency,
+              ),
+              CryptoCard(
+                cryptoCurrency: 'ETH',
+                value: isWaiting ? '?' : coinValues['ETH'],
+                selectedCurrency: selectedCurrency,
+              ),
+              CryptoCard(
+                cryptoCurrency: 'LTC',
+                value: isWaiting ? '?' : coinValues['LTC'],
+                selectedCurrency: selectedCurrency,
+              ),
+            ],
+          ),
+          Container(
+            height: 130.0,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: getPicker(),
+            // child: Platform.isAndroid ? androidDropdown() : iosPicker(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CryptoCard extends StatelessWidget {
+  const CryptoCard({this.value, this.selectedCurrency, this.cryptoCurrency});
+
+  final String? value;
+  final String? selectedCurrency;
+  final String? cryptoCurrency;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=B04D3611-B390-46A2-A72D-47983D432408
